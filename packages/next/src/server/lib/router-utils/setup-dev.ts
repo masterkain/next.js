8b85b1811746774b28d9feb8580d03acacc5429a
Jiachi Liu <inbox@huozhi.im>
2023-08-18 01:13:38 +0200

    remove flags

--- packages/next/src/server/lib/router-utils/setup-dev.ts
+++ packages/next/src/server/lib/router-utils/setup-dev.ts
@@ -727,7 +727,6 @@ async function startWatcher(opts: SetupOpts) {
           matchers?: MiddlewareMatcher[]
         }
       | undefined
-    hasAppNotFound?: boolean
     interceptionRoutes?: ReturnType<
       typeof import('./filesystem').buildCustomRoute
     >[]
@@ -809,7 +808,6 @@ async function startWatcher(opts: SetupOpts) {
       let envChange = false
       let tsconfigChange = false
       let conflictingPageChange = 0
-      let hasRootAppNotFound = true
 
       const { appFiles, pageFiles } = opts.fsChecker
 
@@ -1176,7 +1174,6 @@ async function startWatcher(opts: SetupOpts) {
         : undefined
 
       await propagateToWorkers('middleware', serverFields.middleware)
-      serverFields.hasAppNotFound = hasRootAppNotFound
 
       opts.fsChecker.middlewareMatcher = serverFields.middleware?.matchers
         ? getMiddlewareRouteMatcher(serverFields.middleware?.matchers)
